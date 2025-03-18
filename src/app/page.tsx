1d91dd6561ede737403e9f9588b370264c4b60f8
scngnr <scngnr@gmail.com>
2025-03-18 11:51:55 +0300

    listingcard eklendi. çeviri eklendi

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,43 +4,50 @@ import Container from "@/components/Container";
 import i18n from "../components/language/i18n";
 import { useTranslation } from "react-i18next";
 import EmptyState from "@/components/EmptyState";
-
+import {  useEffect, useState } from "react";
+import { Listing } from "@prisma/client";
+import axios from "axios";
+import ListingCard from "@/components/listings/ListingCard";
 
 export default function Home() {
+  const [listings, setListings] = useState<Listing[]>([]);
+  const { t, i18n: { language } } = useTranslation(); 
 
-  const isEmpty = false;
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        const response = await axios.get('/api/listings');
+        setListings(response.data);
+      } catch (error) {
+        console.error('Veri alınırken hata oluştu:', error);
+      }
+    }
 
-  if(!isEmpty){
+    fetchData();
+  }, [listings] ); // Bağımlılık dizisi boş
+
+  if (!language) { // i18n.language'i doğrudan kullandık
+    return null;
+  }
+
+  if (listings.length === 0) {
     return (
       <ClientOnly>
         <EmptyState showReset />
       </ClientOnly>
-    )
-  }
-
-  
-  const { t } = useTranslation();
-
-  if (!i18n.language) {
-    return null
+    );
   }
 
   return (
     <ClientOnly>
       <Container>
-        <div className="
-            pt-24
-            grid
-            grid-cols-1
-            sm:grid-cols-2
-            md:grid-cols-3
-            lg:grid-cols-4
-            xl:grid-cols-5
-            2xl:grid-cols-6
-            gap-8
-        ">
-          <div>{t('my Future Listings')}</div>
-
+        <div className="pt-24 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 gap-8">
+          {listings.map((listing) => (
+            <ListingCard
+              key={listing.id}
+              
+            />
+          ))}
         </div>
       </Container>
     </ClientOnly>
