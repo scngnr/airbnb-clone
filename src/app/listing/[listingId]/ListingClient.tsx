9b238d03e8659f862ae33c8943c0f09712ff3fb8
scngnr <scngnr@gmail.com>
2025-03-20 18:31:08 +0300

    feat(listing): add ListingInfo component to display listing details
    This commit introduces the ListingInfo component to the ListingClient to display detailed information about the listing, such as user, category, description, and room counts. This enhances the user experience by providing more comprehensive details about the listing.

--- src/app/listing/[listingId]/ListingClient.tsx
+++ src/app/listing/[listingId]/ListingClient.tsx
@@ -4,6 +4,7 @@ import { SafeListing, SafeUser } from "@/app/types";
 import { categories } from "@/components/Navbar/Categories/Categories";
 import Container from "@/components/Container";
 import ListingHead from "@/components/listings/ListingHead";
+import ListingInfo from "@/components/listings/ListingInfo";
 
 
 interface ListingClientProps {
@@ -35,6 +36,17 @@ const ListingClient = ({
                         id={listing.id} 
                         currentUser={currentUser} 
                     />
+                    <div className="grid grid-cols-1 md:grid-cols-7 md:gap-10 mt-6">
+                        <ListingInfo
+                            user={listing.user}
+                            category={category}
+                            description={listing.description}
+                            roomCount={listing.roomCount}
+                            guestCount={listing.guestCount}
+                            bathroomCount={listing.bathroomCount}
+                            locationValue={listing.locationValue}
+                        />
+                    </div>
                 </div>
 
             </div>
