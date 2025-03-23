b534df1e2d1205e922fa0a4930b483bed6a8cffe
scngnr <scngnr@gmail.com>
2025-03-23 14:47:33 +0300

    feat: add ListingsReservation component for handling reservation details
    This commit introduces a new component `ListingsReservation` that manages the reservation details for listings. The component includes a calendar for selecting dates and displays the price per night. This addition is part of the effort to enhance the user interface for booking functionality.

---
+++ src/components/listings/ListingsReservation.tsx
@@ -0,0 +1,44 @@
+"use client";
+
+import Calendar from "../Inputs/Calendar";
+
+interface ListingReservationProps {
+    price: number;
+    dateRange: Range;
+    totalPrice: number;
+    onChangeDate: (dateRange: Range) => void;
+    onSubmit: () => void;
+    disabled: boolean;
+    disabledDates: Date[];
+}
+
+const ListingReservation = ({
+    price,
+    dateRange,
+    totalPrice,
+    onChangeDate,
+    onSubmit,
+    disabled,
+    disabledDates,
+}: ListingReservationProps) => {
+    return (
+        <div className="bg-white rounded-xl border-[1px] border-neutral-200 overflow-hidden ">
+            <div className="flex flex-row items-center gap-1 p-4">
+                <div className="text-2xl font-semibold">
+                    {price}
+                </div>
+                <div className="font-lisgt text-neutral-600">
+                    night
+                </div>
+            </div>
+            <hr/>
+            <Calendar
+                value={dateRange}
+                disabledDates={disabledDates}
+                onChange={(value) => onChangeDate(value.selection)}
+            />
+        </div>
+    );
+}
+
+export default ListingReservation;
